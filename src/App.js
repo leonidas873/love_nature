df01f477c7be5aa161641215959b8467b5f4f618
levan873 <levan.khaburzania@nexus.ge>
2023-03-18 15:04:22 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <MainLayout>
       <Routes>
-        <Route exact path="/love_nature" element={<Home />}></Route>
+        <Route exact path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/services" element={<Services />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
